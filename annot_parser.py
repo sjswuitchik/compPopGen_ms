491be4c47e592ce79a223fdde471a74e390dd815
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2021-01-26 14:08:22 -0500

    Update annot_parser.py

--- annot_parser.py
+++ annot_parser.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python -tt
 
 
-# To run:
+# To run standalone:
 
 #   python annot_parser.py $INSHORT.ann.vcf $INSHORT.ann.bed -key missense_variant -key synonymous_variant
+
+## as re-written here, annot_parser.py is imported into snpEff functions
 
 
 
@@ -26,7 +28,6 @@ def extract_effect(ann, key_effects):
         key_effects: list of effect keywords to potentially extract
         output: synonymous 
         
-        check out the doctest library in Python
         >>> extract_effect('C|synonymous&stop_retained_variant|LOW|id6|GE',['synonymous', 'missense'])
         ['synonymous']
         
@@ -46,51 +47,49 @@ def extract_effect(ann, key_effects):
     # return the found effects as a list
     return list(effect_set)
 
-
-
-def main():
-        parser = argparse.ArgumentParser(description='Do something with VCF files')
-        parser.add_argument('infile', help='Input VCF file.')
-        parser.add_argument('outfile', help='Output .bed file')
-        parser.add_argument('-key',help='Effects to extract. Can be repeated, must be mutually exclusive', action='append')
-        args = parser.parse_args()
-    
-        vcf = VCF(args.infile, gts012=True) # gts012=True makes value of 3 UNKNOWN, with 0,1,2 corresponding to numb ALT alleles
-
-        with open(args.outfile, 'w') as outFile:
-            for variant in tqdm.tqdm(vcf):                    
-                if len(variant.ALT) == 1:
-                    try:
-                        for field in variant.INFO:
-                                if field[0] == 'ANN':
-                                    f1 = field[1]
-                                    effect = {}
-                                    for ann in f1.split(','):
-                                        tmp = extract_effect(ann, args.key)
-                                        if tmp:
-                                            # Found some!
-                                            for found_effect in tmp:
-                                                effect[found_effect] = 1
-                                    if effect:
-                                        # Join the effects after unique-ifying them
-                                        if "missense_variant" in effect:
-                                            effect_str = "missense_variant"
-                                        elif "synonymous_variant" in effect:
-                                            effect_str = "synonymous_variant"
-                                        else:
-                                            raise Exception("Found an effect but it doesn't contain any keys!")
-                                        start_pos = int(variant.end) - 1
-                                        outFile.write('%s\t%s\t%s\t%s\n' % (variant.CHROM, start_pos, variant.end, effect_str))
-                    except Exception as e:
-                        # two keys found in the same annotation.
-                        sys.stderr('Two mutually exclusive effects found at the same time.')
-                        sys.stderr(variant)
-                        
-            outFile.close()
-            
-            
+# feel free to name this something better!
+# This should also adopt the logging approach used in snpeff_func.py to 
+# create a unified output log for this entire pipeline stage
+def proc_file(outfile,infile,key):
+   with open(args.outfile, 'w') as outFile:
+        for variant in tqdm.tqdm(vcf):                    
+            if len(variant.ALT) == 1:
+                try:
+                    for field in variant.INFO:
+                            if field[0] == 'ANN':
+                                f1 = field[1]
+                                effect = {}
+                                for ann in f1.split(','):
+                                    tmp = extract_effect(ann, args.key)
+                                    if tmp:
+                                        # Found some!
+                                        for found_effect in tmp:
+                                            effect[found_effect] = 1
+                                if effect:
+                                    # Join the effects after unique-ifying them
+                                    if "missense_variant" in effect:
+                                        effect_str = "missense_variant"
+                                    elif "synonymous_variant" in effect:
+                                        effect_str = "synonymous_variant"
+                                    else:
+                                        raise Exception("Found an effect but it doesn't contain any keys!")
+                                    start_pos = int(variant.end) - 1
+                                    outFile.write('%s\t%s\t%s\t%s\n' % (variant.CHROM, start_pos, variant.end, effect_str))
+                except Exception as e:
+                    # two keys found in the same annotation.
+                    sys.stderr('Two mutually exclusive effects found at the same time.')
+                    sys.stderr(variant)
+        outFile.close()
+    return outFile
     
 if __name__ == '__main__':
-  main()
+    parser = argparse.ArgumentParser(description='Do something with VCF files')
+    parser.add_argument('infile', help='Input VCF file.')
+    parser.add_argument('outfile', help='Output .bed file')
+    parser.add_argument('-key',help='Effects to extract. Can be repeated, must be mutually exclusive', action='append')
+    args = parser.parse_args()
 
+    vcf = VCF(args.infile, gts012=True) # gts012=True makes value of 3 UNKNOWN, with 0,1,2 corresponding to numb ALT alleles
 
+    proc_file(args.outfile,args.infile,args.key)
+               
