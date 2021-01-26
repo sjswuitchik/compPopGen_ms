43066432371e7ec886e48173050315a0e3a18ced
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2021-01-26 14:00:46 -0500

    Update 03_snpEff_func.py

--- 03_snpEff_func.py
+++ 03_snpEff_func.py
@@ -5,66 +5,51 @@ Created on Tue Jan 19 10:49:24 2021
 
 @author: sjswuitchik
 """
-import os, argparse, subprocess
+import os, argparse, subprocess, shlex, annot_parser
 
-parser = argparse.ArgumentParser(description= 'snpEff commands & cleaning for MK pipeline')
+def annotate_group_VCF(snpEff_path, group_1, group_2, group_out):
+    ''' common function for calling snpEff.jar and annotating groups
+        
+        snpEff_path - directory of snpEff.jar
+        group_1 - 1st arg to snpEff.jar
+        group_2 - 2nd arg to snpEff.jar:   group_2.call.vcf
+        group_out - output file prefix:    group_out.ann.vcf
+    
+    '''
+    output_file = group_out+'ann.vcf'
+    command = 'java -jar '+snpEff_path+'/snpEff.jar ' + group_1 + ' ' + group_2 + '.call.vcf > '+ output_file
+    try:
+        # Run the subprocess, capture its output, throw an exception if there's an error,
+        # and return the output as text not binary strings.
+        p = subprocess.run(shlex.split(command), capture_output=True, check=True, text=True) 
+    except subprocess.CalledProcessError as e:
+        # error in subprocess!
+        raise e 
+ 
+    return output_file     
 
-requiredParam = parser.add_argument_group('required parameters')
-requiredParam.add_argument('-i', type = str, metavar = 'ingroup_short', required = True, help = 'Base name for ingroup species')
-requiredParam.add_argument('-o', type = str, metavar = 'outgroup_short', required = True, help = 'Base name for outgroup species')
-requiredParam.add_argument('-p', type = str, metavar = 'snpEff_path', required = True, help = 'Path for snpEff directory without terminal forward slash')
+def annotate_ingroup_VCF(snpEff_path,ingroup_short):
+    ''' Annotate the ingroup VCF with the ingroup genome as reference'''
+    return annotate_group_VCF(snpEff_path, ingroup_short, ingroup_short, ingroup_short)   
 
-args = parser.parse_args()
+def annotate_outgroup_VCF(snpEff_path,ingroup_short, outgroup_short):
+    '''Annotate the outgroup VCF with the ingroup genome as reference'''
+    return annotate_group_VCF(snpEff_path, ingroup_short, outgroup_short, outgroup_short)   
 
-print('VCF annotation with snpEff')
+def parse_variants(outfile,infile,keys):
+    ''' Description goes here
+        keys should be a list of keys '''
+    return annot_parser.proc_file(outfile,infile,keys)
 
-# annotate ingroup VCF
-command = ('java -jar '+args.p+'/snpEff.jar '+args.i+' '+args.i+'.call.vcf > '+args.i+'ann.vcf')
-print('Ingroup annotating command: '+command)
-p = subprocess.Popen(command, shell = True)
-sts = os.waitpid(p.pid, 0)[1]
 
-# annotate outgroup VCF
-command = ('java -jar '+args.p+'/snpEff.jar '+args.i+' '+args.o+'.call.vcf > '+args.o+'ann.vcf')
-print('Outgroup annotating command: '+command)
-p = subprocess.Popen(command, shell = True)
-sts = os.waitpid(p.pid, 0)[1]
+if __name__=='__main__':
+    parser = argparse.ArgumentParser(description= 'snpEff commands & cleaning for MK pipeline')
 
-# parse variants from annotated VCF to output an annotated BED file 
-## I think this can be done with subprocess.call(), need to check if there is a way to set parameters 
-command = ('python3 annot_parser.py '+args.i+'.ann.vcf '+args.i+'.ann.bed -key missense_variant -key synonymous_variant')
-print('Ingroup parsing command: '+command)
-p = subprocess.Popen(command, shell = True)
-sts = os.waitpid(p.pid, 0)[1]
+    # Note the addition of the key for the annot_parser call
+    requiredParam = parser.add_argument_group('required parameters')
+    requiredParam.add_argument('-i', type = str, metavar = 'ingroup_short', required = True, help = 'Base name for ingroup species')
+    requiredParam.add_argument('-o', type = str, metavar = 'outgroup_short', required = True, help = 'Base name for outgroup species')
+    requiredParam.add_argument('-p', type = str, metavar = 'snpEff_path', required = True, help = 'Path for snpEff directory without terminal forward slash')
+    requiredParam.add_argument('-key',help='Effects to extract. Can be repeated, must be mutually exclusive', action='append')
 
-command = ('python3 annot_parser.py '+args.o+'.ann.vcf '+args.o+'.ann.bed -key missense_variant -key synonymous_variant')
-print('Outgroup parsing command: '+command)
-p = subprocess.Popen(command, shell = True)
-sts = os.waitpid(p.pid, 0)[1]
-
-# create onlyCDS.bed from GFF 
-command = ('column -s, -t < genes.gff | awk \'$3 == "CDS"\' > onlyCDS.gff\n'
-           'awk -f gff2bed.awk onlyCDS.gff > onlyCDS.bed\n'
-           'cat onlyCDS.bed | python genenames.py > onlyCDS.genes.bed')
-p = subprocess.Popen(command, shell = True)
-sts = os.waitpid/(p.pid, 0)[1]
-
-# associate genes with annotations in ingroup
-command = ('bedtools intersect -a '+args.i+'.ann.bed -b onlyCDS.genes.bed -ab | cut -f1,2,3,4,8 | bedtools merge -i - -d -1 -c 4,5 -o distinct > '+args.i+'.final.bed')
-print('Ingroup intersect command: '+command)
-p = subprocess.Popen(command, shell = True)
-sts = os.waitpid(p.pid, 0)[1]
-
-# associate genes with annotations in outgroup
-command = ('bedtools intersect -a '+args.o+'.ann.bed -b onlyCDS.genes.bed -ab | cut -f1,2,3,4,8 | bedtools merge -i - -d -1 -c 4,5 -o distinct > '+args.o+'.final.bed')
-print('Outgroup intersect command: '+command)
-p = subprocess.Popen(command, shell = True)
-sts = os.waitpid(p.pid, 0)[1]
-
-# associate callable sites with genes 
-command = ('bedtools intersect -a callable.bed -b onlyCDS.genes.bed -wb | cut -f1,2,3,7 | bedtools sort -i - | bedtools merge -i - -c 4 -o distinct > callable.cds.bed')
-p = subprocess.Popen(command, shell = True)
-sts = os.waitpid(p.pid, 0)[1]
-
-print('\nFinished!\n\n')
-
+    args = parser.parse_args()
