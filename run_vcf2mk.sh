2b30acf821c9c64224f0cfbd38bf4e4b2e9d9c46
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2021-02-04 15:21:45 -0500

    Create run_vcf2mk.sh

---
+++ run_vcf2mk.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH -J sm
+#SBATCH -o out
+#SBATCH -e err
+#SBATCH -p holy-info
+#SBATCH -n 1
+#SBATCH -t 9000
+#SBATCH --mem=10000
+
+
+conda activate snakemake
+snakemake --snakefile Snakefile_vcf2mk --profile ./profiles/slurm
