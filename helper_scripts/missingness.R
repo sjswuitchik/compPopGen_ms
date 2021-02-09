9018bd19bcd65023c82273f5041e316b04d14a1d
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2021-02-09 15:10:06 -0500

    Update missingness.R

--- helper_scripts/missingness.R
+++ helper_scripts/missingness.R
@@ -9,21 +9,22 @@ if (length(args)==0) {
   stop("At least one argument must be supplied (input file).\n", call.=FALSE)
 } 
 
-ingroup <- read.delim(args[1]) %>%
+ingroup <- read_delim(args[1], ) %>%
   group_by(INDV) %>%
   summarise_each(funs(sum)) %>%
   mutate(missing = N_MISS/N_DATA)
 threshold <- 3*median(ingroup$missing)
 remove <- ingroup %>% 
-  filter(missing >= threshold)
-write.csv(remove %>% select(INDV), "ingroup.remove.indv", row.names = F, quote = F)
+  filter(missing >= threshold) %>%
+  select(INDV) %>%
+  write_delim(., "ingroup.remove.indv", delim = '\t', col_names = T)
 
-outgroup <- read.delim(args[2]) %>%
+outgroup <- read_delim(args[2]) %>%
   group_by(INDV) %>%
   summarise_each(funs(sum)) %>%
   mutate(missing = N_MISS/N_DATA)
 threshold <- 3*median(outgroup$missing)
 remove <- outgroup %>% 
-  filter(missing >= threshold)
-write.csv(remove %>% select(INDV), "outgroup.remove.indv", row.names = F, quote = F)
-
+  filter(missing >= threshold) %>%
+  select(INDV) %>%
+  write_delim(., "outgroup.remove.indv", delim = '\t', col_names = T)
