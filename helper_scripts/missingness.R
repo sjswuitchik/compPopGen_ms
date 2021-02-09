ecee77edd5f57c4102f722d9c5d6c52cd634154a
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2021-02-09 09:46:23 -0500

    Update missingness.R

--- helper_scripts/missingness.R
+++ helper_scripts/missingness.R
@@ -2,15 +2,8 @@
 
 library(tidyverse)
 
-args <- commandArgs(trailingOnly = T)
-
-# test if there is at least one argument: if not, return an error
-if (length(args)==0) {
-  stop("At least one argument must be supplied (input file).\n", call.=FALSE)
-} 
-
 # read in data and calculate missingness
-ingroup <- read.delim(args[1]) %>%
+ingroup <- read.delim("ingroup_missing_data.txt", delim = "\t", col_names = T) %>%
   group_by(INDV) %>%
   summarise_each(funs(sum)) %>%
   mutate(missing = N_MISS/N_DATA)
@@ -19,7 +12,7 @@ remove <- ingroup %>%
   filter(missing >= threshold)
 write.csv(remove %>% select(INDV), "ingroup.remove.indv", row.names = F, quote = F)
 
-outgroup <- read.delim(args[2]) %>%
+outgroup <- read.delim("outgroup_missing_data.txt", delim = "\t", col_names = T) %>%
   group_by(INDV) %>%
   summarise_each(funs(sum)) %>%
   mutate(missing = N_MISS/N_DATA)
