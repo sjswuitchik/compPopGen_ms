383d63ac6a6c65307a089d6a789892061aabeebd
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2021-03-10 10:20:13 -0500

    Update run_genomecov.sh

--- VCFqc/vcfCleanup/run_genomecov.sh
+++ VCFqc/vcfCleanup/run_genomecov.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-#SBATCH -J concatVCF
-#SBATCH -e vcfs/logs/slurm-%j.err
-#SBATCH -o vcfs/logs/slurm-%j.out
+#SBATCH -J bedgraphs
+#SBATCH -e coverage/logs/slurm-%j.err
+#SBATCH -o coverage/logs/slurm-%j.out
 #SBATCH -p shared
 #SBATCH -n 1
 #SBATCH -t 02-00:00:00
 #SBATCH --mem=8000
 
 # run from /n/holylfs/LABS/informatics/ashultz/CompPopGen/SPECIES_DATASETS/gatherVCFs_dir 
-# sbatch run_concatVCFs.sh spp_name
+# sbatch run_genomecov.sh spp_name
 
 module load bedtools2/2.26.0-fasrc01
 
