bf5f59e1efdc4d3f38faff5eef5a5a74e47543c7
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2021-03-11 13:02:11 -0500

    Create vcfLineCheck.sh

---
+++ VCFqc/cleanup/vcfLineCheck.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# run from /n/holylfs/LABS/informatics/ashultz/CompPopGen/SPECIES_DATASETS/gatherVCFs_dir/vcfs
+# ./chromSizes.sh spp_name
+
+zgrep -v '^#' $1_combined.vcf.gz | wc -l > $1.log
+zgrep -v '^#' ../../$1/vcf/*.vcf.gz | wc -l >> $1_split.log
