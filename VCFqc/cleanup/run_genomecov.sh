41a6e45eaef9723bc2447502c28c07d239fec7bb
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2021-03-11 10:29:31 -0500

    Update run_genomecov.sh

--- VCFqc/cleanup/run_genomecov.sh
+++ VCFqc/cleanup/run_genomecov.sh
@@ -7,13 +7,13 @@
 #SBATCH -t 02-00:00:00
 #SBATCH --mem=8000
 
-# run from /n/holylfs/LABS/informatics/ashultz/CompPopGen/SPECIES_DATASETS/spp_name/dedup/
+# run from /n/holylfs/LABS/informatics/ashultz/CompPopGen/SPECIES_DATASETS/gatherVCFs_dir
 # sbatch run_genomecov.sh spp_name
 
 module load bedtools2/2.26.0-fasrc01
 
-for file in *.dedup.sorted.bam;
+for file in ../$1/dedup/*.dedup.sorted.bam;
 do
-  bedtools genomecov -bga -ibam $file -g /n/holylfs/LABS/informatics/ashultz/CompPopGen/SPECIES_DATASETS/gatherVCFs_dir/coverage/$1.chrom.sizes > /n/holylfs/LABS/informatics/ashultz/CompPopGen/SPECIES_DATASETS/gatherVCFs_dir/coverage/$file.bg
-  bedtools genomecov -ibam $file -g /n/holylfs/LABS/informatics/ashultz/CompPopGen/SPECIES_DATASETS/gatherVCFs_dir/coverage/$1.chrom.sizes > /n/holylfs/LABS/informatics/ashultz/CompPopGen/SPECIES_DATASETS/gatherVCFs_dir/coverage/$file.hist
+  bedtools genomecov -bga -ibam $file -g /n/holylfs/LABS/informatics/ashultz/CompPopGen/SPECIES_DATASETS/gatherVCFs_dir/coverage/$1.chrom.sizes > $file.bg
+  bedtools genomecov -ibam $file -g /n/holylfs/LABS/informatics/ashultz/CompPopGen/SPECIES_DATASETS/gatherVCFs_dir/coverage/$1.chrom.sizes > $file.hist
 done
