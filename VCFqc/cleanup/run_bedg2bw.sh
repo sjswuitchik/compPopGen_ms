3e8670ca8e38f5bac6509df27f97cdd368e782e0
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2021-03-12 15:12:27 -0500

    Update run_bedg2bw.sh

--- VCFqc/cleanup/run_bedg2bw.sh
+++ VCFqc/cleanup/run_bedg2bw.sh
@@ -5,7 +5,7 @@
 #SBATCH -p shared
 #SBATCH -n 1
 #SBATCH -t 02-00:00:00
-#SBATCH --mem=8000
+#SBATCH --mem=10000
 
 # run from /n/holylfs/LABS/informatics/ashultz/CompPopGen/SPECIES_DATASETS/gatherVCFs_dir/coverage
 # sbatch run_bedg2bw.sh spp_name
