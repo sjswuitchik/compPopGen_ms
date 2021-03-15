e5b7dcc7a45dd71b7beb0c2f4555e09e6e1ec475
sjswuitchik <sjswuit@g.harvard.edu>
2021-03-15 10:10:18 -0400

    Update run_moveBedgraphs.sh

--- VCFqc/cleanup/run_moveBedgraphs.sh
+++ VCFqc/cleanup/run_moveBedgraphs.sh
@@ -4,11 +4,11 @@
 #SBATCH -e gatherVCFs_dir/coverage/logs/err_%j
 #SBATCH -p shared
 #SBATCH -n 1
-#SBATCH -t 48:00:00
-#SBATCH --mem=8000
+#SBATCH -t 00:30:00
+#SBATCH --mem=4000
 
 # run from /n/holylfs/LABS/informatics/ashultz/CompPopGen/SPECIES_DATASETS
 # sbatch moveBedgraphs.sh spp_name
 
-cd $1/dedup
+cd $1dedup
 mv *.bg /n/holylfs/LABS/informatics/ashultz/CompPopGen/SPECIES_DATASETS/gatherVCFs_dir/coverage/$1
