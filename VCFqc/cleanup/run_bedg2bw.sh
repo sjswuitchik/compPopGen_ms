f67d13889b53f15da537b564549a89894c563a43
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2021-03-15 11:25:11 -0400

    Update run_bedg2bw.sh

--- VCFqc/cleanup/run_bedg2bw.sh
+++ VCFqc/cleanup/run_bedg2bw.sh
@@ -14,5 +14,5 @@ for file in $1/*.bg;
 do
   sort -k1,1 -k2,2n $file > $file.sorted.bg
   ./brename -p ".dedup.sorted.bam.bg.sorted.bg" -r ".bg" -R
-  ./bedGraphToBigWig $file.sorted.bg $1.chrom.sizes $file.bw
+  ./bedGraphToBigWig $file.bg $1.chrom.sizes $file.bw
 done
