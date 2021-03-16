69017db7742184da8a8ed1576b038682e961bdba
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2021-03-16 15:08:24 -0400

    Update and rename run_bedg2bw.sh to run_bedgSort.sh

--- VCFqc/cleanup/run_bedgSort.sh
+++ VCFqc/cleanup/run_bedgSort.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH -J bedg2bw
+#SBATCH -J bedgSort
 #SBATCH -e logs/slurm-%j.err
 #SBATCH -o logs/slurm-%j.out
 #SBATCH -p shared
@@ -8,11 +8,10 @@
 #SBATCH --mem=10000
 
 # run from /n/holylfs/LABS/informatics/ashultz/CompPopGen/SPECIES_DATASETS/gatherVCFs_dir/coverage
-# sbatch run_bedg2bw.sh spp_name
+# sbatch run_bedgSort.sh spp_name
 
 for file in $1/*.bg;
 do
   sort -k1,1 -k2,2n $file > $file.sorted
   ./brename -p ".dedup.sorted.bam.bg.sorted" -r ".bg" -R
-  #./bedGraphToBigWig $file.bg $1.chrom.sizes $file.bw ##nb: fixing this line with some testing
 done
