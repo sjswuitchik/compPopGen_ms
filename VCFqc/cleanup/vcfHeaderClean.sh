6fe17a3cf31cee9a6680f9d9861a0274bf60963b
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2021-03-17 16:22:01 -0400

    Update vcfHeaderClean.sh

--- VCFqc/cleanup/vcfHeaderClean.sh
+++ VCFqc/cleanup/vcfHeaderClean.sh
@@ -10,6 +10,7 @@ zcat $1_updatedFilter.vcf.gz | \
 sed -e 's/##FILTER=<ID=LowQual,Description="Low quality">/#/g' | \
 sed -e 's/##FILTER=<ID=GATK_default/#/g' | \
 sed -e 's/##FILTER=<ID=FS_SOR_filter,Description="(vc.isSNP() && ((vc.hasAttribute('FS') && FS > 60.0) || (vc.hasAttribute('SOR') &&  SOR > 3.0))) || ((vc.isIndel() || vc.isMixed()) && ((vc.hasAttribute('FS') && FS > 200.0) || (vc.hasAttribute('SOR') &&  SOR > 10.0)))">/##FS_SOR_filter,Description="filter SNPs for strand bias with Phred-scaled p-value for Fisher's exact test above 60 and Symmetric Odds Ratio above 3; or indels; or if mixed, a Phred-scaled p-value above 200 and a Symmetric Odds Ratio above 10"/g' | \
+sed -e 's/##FILTER=<ID=MQ_filter,Description="vc.isSNP() && ((vc.hasAttribute('MQ') && MQ < 40.0) || (vc.hasAttribute('MQRankSum') && MQRankSum < -12.5))">/##MQ_filter,Description="filter SNPs with RMS mapping quality less than 40 and Z-score for Wilcoxon rank sum test less than -12.5"/g' | \
 sed -e 's//g' | \
 > $1_final.clean.vcf.gz
 
