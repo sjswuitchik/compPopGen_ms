0202972b1ef90441310991a68df6ca2567a3da08
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2021-03-18 16:50:13 -0400

    Update vcfHeaderClean.sh

--- VCFqc/cleanup/vcfHeaderClean.sh
+++ VCFqc/cleanup/vcfHeaderClean.sh
@@ -11,7 +11,7 @@ sed -e 's/^##FILTER=<ID=LowQual,Description="Low quality">/#/g' \
 -e $'s/^##FILTER=<ID=GATK_default,Description="vc.isSNP() && ((vc.hasAttribute(\'MQ\') && MQ < 40.0) || (vc.hasAttribute(\'MQRankSum\') && MQRankSum < -12.5))">/#/g' \
 -e '/^##FILTER=<ID=FS_SOR_filter,Description=/s/.*/##FS_SOR_filter,Description="filter SNPs for strand bias with Phred-scaled p-value for Fishers exact test above 60 and Symmetric Odds Ratio above 3; or indels; or if mixed, a Phred-scaled p-value above 200 and a Symmetric Odds Ratio above 10"/' \
 -e '/^##FILTER=<ID=MQ_filter,Description=/s/.*/##MQ_filter,Description="filter SNPs with RMS mapping quality less than 40 and Z-score for Wilcoxon rank sum test for read mapping quality less than -12.5"/' \
--e '/^##FILTER=<ID=RPRS_filter,Description=/s/.*/##RPRS_filter,Description="filter SNPs with Z-Score for Wilcoxon rank sum test for read position bias less than -8; or indels; or if mixed, a Z-Score for Wilcoxon rank sum test less than -20"/' \
+-e '/^##FILTER=<ID=RPRS_filter,Description=/s/.*/##RPRS_filter,Description="filter SNPs with Z-Score for Wilcoxon rank sum test for read position bias less than -8; or indels; or if mixed, a Z-Score for Wilcoxon rank sum test less than -20"/' | \
 bgzip -c > $1_final.clean.vcf.gz
 
 bcftools index -t $1_final.clean.vcf.gz
