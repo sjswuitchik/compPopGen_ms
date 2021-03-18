10a07339680c20b4ea62068c48521a66c0483025
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2021-03-18 14:52:21 -0400

    Update run_gatkUpdate_cpg.sh

--- VCFqc/cleanup/run_gatkUpdate_cpg.sh
+++ VCFqc/cleanup/run_gatkUpdate_cpg.sh
@@ -5,7 +5,7 @@
 #SBATCH -p shared
 #SBATCH -n 1
 #SBATCH -t 24:00:00
-#SBATCH --mem=8000
+#SBATCH --mem=12000
 
 # run from /n/holylfs/LABS/informatics/ashultz/CompPopGen/SPECIES_DATASETS/gatherVCFs_dir/vcfs
 # sbatch run_gatkUpdate.sh spp_name
