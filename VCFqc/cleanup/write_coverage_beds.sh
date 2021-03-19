565de5dc83e727543a9c2243c5095f6a1eafdfe5
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2021-03-19 17:36:33 -0400

    Update write_coverage_beds.sh

--- VCFqc/cleanup/write_coverage_beds.sh
+++ VCFqc/cleanup/write_coverage_beds.sh
@@ -10,6 +10,8 @@
 # run from /n/holylfs/LABS/informatics/ashultz/CompPopGen/SPECIES_DATASETS/gatherVCFs_dir/coverage
 # sbatch write_coverage_beds.sh spp_name
 
+set -o errexit
+
 cd $1/
 mean=$(awk '{sum = sum+$4}{size=size+$2}{avg=sum/size}END{print avg}' $1.summary.tab)
 
