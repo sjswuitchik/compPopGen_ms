a8571a2670b68e7bab06a50506cb94fa6487e49d
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2021-03-19 12:02:31 -0400

    Create write_coverage_beds.sh

---
+++ VCFqc/cleanup/write_coverage_beds.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH -J covBeds
+#SBATCH -o logs/slurm-%j
+#SBATCH -e logs/slurm-%j
+#SBATCH -p shared
+#SBATCH -n 1
+#SBATCH -t 24:00:00
+#SBATCH --mem=8000
+
+# run from /n/holylfs/LABS/informatics/ashultz/CompPopGen/SPECIES_DATASETS/gatherVCFs_dir/coverage
+# sbatch write_coverage_beds.sh spp_name 
+
+## untested as written here 
+
+cd $1/
+gzip -dc $1.merge.bg.gz | awk -v "avg=$2" "spp=$1" -f sum_cov.awk
