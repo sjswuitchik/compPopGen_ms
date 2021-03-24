477e073c24b7014530510eb539cafb67fc2e7e8b
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2021-03-24 16:47:47 -0400

    Update run_compressBedg.sh

--- VCFqc/cleanup/run_compressBedg.sh
+++ VCFqc/cleanup/run_compressBedg.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #SBATCH -J bgMove
-#SBATCH -o gatherVCFs_dir/coverage/logs/out_%j
-#SBATCH -e gatherVCFs_dir/coverage/logs/err_%j
+#SBATCH -o logs/slurm-%j
+#SBATCH -e logs/slurm-%j
 #SBATCH -p shared
 #SBATCH -n 1
-#SBATCH -t 00:30:00
-#SBATCH --mem=4000
+#SBATCH -t 24:00:00
+#SBATCH --mem=8000
 
 # run from /n/holylfs/LABS/informatics/ashultz/CompPopGen/SPECIES_DATASETS/gatherVCFs_dir/coverage
 # sbatch run_compressBedg.sh spp_name
