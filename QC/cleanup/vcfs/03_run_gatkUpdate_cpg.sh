f9af0785fb909ea0ff030972d1796212de6c0786
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2021-03-25 12:24:31 -0400

    Update 03_run_gatkUpdate_cpg.sh

--- QC/cleanup/vcfs/03_run_gatkUpdate_cpg.sh
+++ QC/cleanup/vcfs/03_run_gatkUpdate_cpg.sh
@@ -23,3 +23,6 @@ picard SortVcf -Xmx8g --TMP_DIR=$PWD/tmp -I $1_combined.rep.vcf.gz -O $1_comboSo
 gatk IndexFeatureFile -I $1_comboSorted.vcf.gz
 
 gatk VariantFiltration -R /n/holylfs/LABS/informatics/ashultz/CompPopGen/SPECIES_DATASETS/$1/genome/$1.fa -V $1_comboSorted.vcf.gz --output $1_updatedFilter.vcf.gz --filter-name "RPRS_filter" --filter-expression "(vc.isSNP() && (vc.hasAttribute('ReadPosRankSum') && ReadPosRankSum < -8.0)) || ((vc.isIndel() || vc.isMixed()) && (vc.hasAttribute('ReadPosRankSum') && ReadPosRankSum < -20.0)) || (vc.hasAttribute('QD') && QD < 2.0)" --filter-name "FS_SOR_filter" --filter-expression "(vc.isSNP() && ((vc.hasAttribute('FS') && FS > 60.0) || (vc.hasAttribute('SOR') &&  SOR > 3.0))) || ((vc.isIndel() || vc.isMixed()) && ((vc.hasAttribute('FS') && FS > 200.0) || (vc.hasAttribute('SOR') &&  SOR > 10.0)))" --filter-name "MQ_filter" --filter-expression "vc.isSNP() && ((vc.hasAttribute('MQ') && MQ < 40.0) || (vc.hasAttribute('MQRankSum') && MQRankSum < -12.5))"
+
+
+## NB: not needed when refactoring to snakemake (bc filter description should already be updated in main pipeline)
