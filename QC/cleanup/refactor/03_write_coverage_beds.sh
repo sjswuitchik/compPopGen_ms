a92d1478666a9d3364a6dc0c2129d27e9f19aff3
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2021-03-25 15:41:12 -0400

    Create 03_write_coverage_beds.sh

---
+++ QC/cleanup/refactor/03_write_coverage_beds.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH -J covBeds
+#SBATCH -o logs/slurm-%j
+#SBATCH -e logs/slurm-%j
+#SBATCH -p shared
+#SBATCH -n 1
+#SBATCH -t 24:00:00
+#SBATCH --mem=8000
+
+# sbatch write_coverage_beds.sh spp_name
+
+set -o errexit
+
+mean=$(awk '{sum = sum+$4}{size=size+$2}{avg=sum/size}END{print avg}' $1/$1.summary.tab)
+
+gzip -dc $1/$1.merge.bg.gz | awk -v avg="$mean" -v spp=$1 -f sum_cov.awk
