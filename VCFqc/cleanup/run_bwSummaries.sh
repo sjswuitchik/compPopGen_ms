1da001fc05d9905413f316536e93e06246ef08cd
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2021-03-25 09:31:55 -0400

    Update run_bwSummaries.sh

--- VCFqc/cleanup/run_bwSummaries.sh
+++ VCFqc/cleanup/run_bwSummaries.sh
@@ -16,8 +16,8 @@ cd $1/
 
 awk 'BEGIN{FS=OFS="\t"}{print $1, 0, $2, $1}' $1.chrom.sizes > $1.genome.bed
 
-.././bedGraphToBigWig $1/$1.merge.bg $1.chrom.sizes $1.merge.bw
+.././bedGraphToBigWig $1.merge.bg $1.chrom.sizes $1.merge.bw
 
-.././bigWigAverageOverBed $1/$1.merge.bw $1.genome.bed $1.summary.tab
+.././bigWigAverageOverBed $1.merge.bw $1.genome.bed $1.summary.tab
 
 gzip $1.merge.bg
