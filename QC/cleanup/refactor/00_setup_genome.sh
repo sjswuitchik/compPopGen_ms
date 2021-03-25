8f02cfa2ee9c8721b7f529d592685cc3e3532ff2
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2021-03-25 15:29:56 -0400

    Update 00_setup_genome.sh

--- QC/cleanup/refactor/00_setup_genome.sh
+++ QC/cleanup/refactor/00_setup_genome.sh
@@ -1,1 +1,14 @@
 #!/bin/bash
+#SBATCH -J genome_set
+#SBATCH -o logs/out_%j
+#SBATCH -e logs/err_%j
+#SBATCH -p shared
+#SBATCH -n 1
+#SBATCH -t 00:30:00
+#SBATCH --mem=4000
+
+# sbatch 00_setup_genome.sh spp_name
+
+faToTwoBit $1/genome/$1.fa $1/$1.2bit
+twoBitInfo $1/$1.2bit stdout | sort -k2rn > $1/$1.chrom.sizes
+awk 'BEGIN{FS=OFS="\t"}{print $1, 0, $2, $1}' $1/$1.chrom.sizes > $1/$1.genome.bed
