aa65a37bcf6a0619ddcd0a53870ff554b50f47a9
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2021-03-28 14:01:46 -0400

    Update 03_run_moveBedgraphs.sh

--- QC/cleanup/coverage/03_run_moveBedgraphs.sh
+++ QC/cleanup/coverage/03_run_moveBedgraphs.sh
@@ -8,7 +8,7 @@
 #SBATCH --mem=4000
 
 # run from /n/holylfs/LABS/informatics/ashultz/CompPopGen/SPECIES_DATASETS
-# sbatch moveBedgraphs.sh spp_name
+# sbatch moveBedgraphs.sh spp_name/
 
 cd $1dedup
 mv *.bg /n/holylfs/LABS/informatics/ashultz/CompPopGen/SPECIES_DATASETS/gatherVCFs_dir/coverage/$1
