b33861219ee05003aa4fda58bffaf2dd922b75e3
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2021-03-31 09:14:42 -0400

    Update 01_genomecov.sh

--- QC/cleanup/refactor/01_genomecov.sh
+++ QC/cleanup/refactor/01_genomecov.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH -J genomecov
-#SBATCH -e logs/slurm-%j.err
-#SBATCH -o logs/slurm-%j.out
+#SBATCH -e logs/err_%j
+#SBATCH -o logs/out_%j
 #SBATCH -p shared
 #SBATCH -n 1
 #SBATCH -t 05-00:00:00
