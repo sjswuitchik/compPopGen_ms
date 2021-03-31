b82c09d9e63c83b4f2c3d006de5147d4c6bd02af
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2021-03-31 09:15:04 -0400

    Update 00_setup_genome.sh

--- QC/cleanup/refactor/00_setup_genome.sh
+++ QC/cleanup/refactor/00_setup_genome.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH -J genome_set
-#SBATCH -o logs/out_%j.out
-#SBATCH -e logs/err_%j.err
+#SBATCH -o logs/out_%j
+#SBATCH -e logs/err_%j
 #SBATCH -p shared
 #SBATCH -n 1
 #SBATCH -t 00:30:00
