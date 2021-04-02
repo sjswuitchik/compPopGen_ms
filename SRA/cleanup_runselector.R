1961331a308ea7ef40ef7f5f6a06f79b40fea97a
Tim Sackton <tsackton@g.harvard.edu>
2021-04-02 11:26:36 -0400

    Fix runselector helper script

--- SRA/cleanup_runselector.R
+++ SRA/cleanup_runselector.R
@@ -1,4 +1,4 @@
-#this is a simple script to take a BioProject downloaded from RunSelector, and make it compatible with the clean metadata pipeline. 
+#this is a simple script to take a BioProject downloaded from RunSelector, and make it compatible with the clean metadata pipeline.
 
 #ideally this should be refactored into functions shared with the SRA_parser.R script
 
@@ -12,9 +12,12 @@ read_sra_full <- function(file, path) {
   df<-read_csv(paste0(path, "/", file), cols(.default="c"), col_names = TRUE)
 }
 
-write_sample_metadata <- function(df, path) {
-  df %>% select(-Run) %>% distinct() %>% 
-  mutate(Organism = str_replace_all(Organism, " ", "_")) %>%
+
+not_all_na <- function(x) {!all(is.na(x))}
+
+write_sample_metadata <- function(df) {
+  df %>%
+  mutate(Organism = str_replace_all(Organism, " ", "_")) %>% distinct() %>%
   mutate(sex = case_when(
     is.na(sex) ~ "missing",
     sex == "male and female" | sex == "mixed" | sex == "pooled male and female" ~ "pooled",
@@ -22,19 +25,13 @@ write_sample_metadata <- function(df, path) {
     tolower(sex) == "not collected" | tolower(sex) == "not determined" ~ "unknown",
     TRUE ~ sex
   )) %>%
+  mutate(`Library Name` = case_when(
+    is.na(`Library Name`) ~ str_c("library_", `Sample Name`),
+    TRUE ~ `Library Name`
+  )) %>%
   split(., .$BioProject) %>%
   imap(~ write_tsv(select_if(as.data.frame(.x), not_all_na), file = str_c(path_to_write, '/SRA-sample-metadata/SRA_Metadata_', .y, '.tsv')))
 }
 
-write_run_metadata <- function(df, path) {
-  df %>% select(Organism, BioProject, BioSample, Run) %>% 
-    mutate(Organism = str_replace_all(Organism, " ", "_")) %>%
-    distinct() %>%
-    split(., .$BioProject) %>%
-    imap(~ write_tsv(as.data.frame(.x), file = str_c(path_to_write, '/SRA-run-metadata/SRA_Run_', .y, '.tsv')))
-  
-}
-
 temp<-read_sra_full("SRA_Metadata_PRJEB2984.txt", "~/Downloads")
 write_sample_metadata(temp, "~/Projects/popgen/compPopGen_ms/SRA")
-write_run_metadata(temp, "~/Projects/popgen/compPopGen_ms/SRA")
