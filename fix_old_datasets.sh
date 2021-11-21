820b157e5508fddf5bc16df6aa91972938768286
Tim Sackton <tsackton@oeb.harvard.edu>
2021-11-21 10:49:50 -0500

    fix old datasets script bugfixes

--- fix_old_datasets.sh
+++ fix_old_datasets.sh
@@ -3,16 +3,16 @@
 SPECIES="Athene_cunicularia"
 OLDDIR="/n/holylfs05/LABS/informatics/Lab/holylfs/ashultz/CompPopGen/SPECIES_DATASETS/Acunicularia"
 
-REFGENOME=$(cut -f3,3 -d, ../samplesheets/${SPECIES}_run_metadata.csv | sed '1d' | sort | uniq)
+REFGENOME=$(cut -f3,3 -d, ${SPECIES}_run_metadata.csv | sed '1d' | sort | uniq)
 
 #check ref genomes -- assume there an accession assembly stats file like GCF_000247815.1_FicAlb1.5_assembly_stats.txt
 
-OLDGENOME = $(ls $OLDDIR/genome/{$REFGENOME}*_assembly_stats.txt)
-if [ ${#ArrayName[@]} != 1 ]; then
+OLDGENOME=$(ls $OLDDIR/genome/${REFGENOME}*_assembly_stats.txt)
+if [ ${#OLDGENOME[@]} != 1 ]; then
   exit 1
 fi
 
-SAMPLES=$(cut -f1,1 -d, ../samplesheets/${SPECIES}_run_metadata.csv | sed '1d')
+SAMPLES=$(cut -f1,1 -d, ${SPECIES}_run_metadata.csv | sed '1d')
 
 mkdir -p data/genome
 mkdir -p data/fastq
@@ -33,12 +33,12 @@ done
 
 OLDINT=$(ls $OLDDIR/genome/*_interval_lists/*.interval_list)
 INTNUM=1
-for interval in OLDINT;
+for interval in $OLDINT;
 do
    INTIDTEMP=${interval%.interval_list}
    INTID=${INTIDTEMP##*_}
    cp --preserve=timestamps $interval results/${SPECIES}/${REFGENOME}/intervalFiles/list${INTID}.list
-   INTNUM++
+   INTNUM=$INTNUM+1
 done
 cat results/${SPECIES}/${REFGENOME}/intervalFiles/list*.list > results/${SPECIES}/${REFGENOME}/intervalFiles/${REFGENOME}_intervals_fb.bed
 #set timestamp on bed file to the timestamp of the first interval file
@@ -48,12 +48,12 @@ touch -r results/${SPECIES}/${REFGENOME}/intervalFiles/list1.list results/${SPEC
 
 for samp in $SAMPLES
 do
-  cp --preserve=timestamps $OLDDIR/dedup/$samp.dedup.sorted.bam results/${SPECIES}/${REFGENOME}/01_mappedReads/${samp}_final.bam
-  cp --preserve=timestamps $OLDDIR/dedup/$samp.dedup.sorted.bai results/${SPECIES}/${REFGENOME}/01_mappedReads/${samp}_final.bai
+  cp -v --preserve=timestamps $OLDDIR/dedup/$samp.dedup.sorted.bam results/${SPECIES}/${REFGENOME}/01_mappedReads/${samp}_final.bam
+  cp -v --preserve=timestamps $OLDDIR/dedup/$samp.dedup.sorted.bai results/${SPECIES}/${REFGENOME}/01_mappedReads/${samp}_final.bai
   mkdir -p results/${SPECIES}/${REFGENOME}/03_gvcfs/$samp
-  for ((i=1, i<=$INTNUM, $i++)); do
-    cp --preserve=timestamps $OLDDIR/gvcf/${samp}.${i}.g.vcf.gz results/${SPECIES}/${REFGENOME}/03_gvcfs/$samp/L${i}.raw.g.vcf.gz
-    cp --preserve=timestamps $OLDDIR/gvcf/${samp}.${i}.g.vcf.gz.tbi results/${SPECIES}/${REFGENOME}/03_gvcfs/$samp/L${i}.raw.g.vcf.gz.tbi
+  for ((i=1; i<=$INTNUN; i++)); do
+    cp -v --preserve=timestamps $OLDDIR/gvcf/${samp}.${i}.g.vcf.gz results/${SPECIES}/${REFGENOME}/03_gvcfs/$samp/L${i}.raw.g.vcf.gz
+    cp -v --preserve=timestamps $OLDDIR/gvcf/${samp}.${i}.g.vcf.gz.tbi results/${SPECIES}/${REFGENOME}/03_gvcfs/$samp/L${i}.raw.g.vcf.gz.tbi
     touch -r results/${SPECIES}/${REFGENOME}/03_gvcfs/$samp/L${i}.raw.g.vcf.gz.tbi results/${SPECIES}/${REFGENOME}/03_gvcfs/$samp/L${i}.done
   done
 done
