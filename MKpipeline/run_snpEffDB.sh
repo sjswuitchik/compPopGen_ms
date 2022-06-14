3fffd2893799a80b709ef53a197aed2ff8657562
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2022-06-14 16:42:05 -0700

    Create run_snpEffDB.sh

---
+++ MKpipeline/run_snpEffDB.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH -J sm
+#SBATCH -o out
+#SBATCH -e err
+#SBATCH -p holy-info
+#SBATCH -n 1
+#SBATCH -t 9000
+#SBATCH --mem=10000
+
+source activate snakemake
+
+snakemake --snakefile Snakefile_snpEffDB --profile ./profiles/slurm
